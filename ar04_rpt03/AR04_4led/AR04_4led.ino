4c4f6a5972f11411f7171a55ee2896295834701b
yoonho0624 <79964431+yoonho0624@users.noreply.github.com>
2021-03-24 16:13:33 +0900

    Add files via upload

---
+++ ar04_rpt03/AR04_4led/AR04_4led.ino
@@ -0,0 +1,31 @@
+/*
+ Dimming 4 leds
+ */
+
+int ledR = 3;    // LED connected to digital pin 3
+int ledG = 5;
+int ledB = 6;
+int ledY = 9;
+
+int dimTime = 20;
+
+void setup()  { 
+  // nothing happens in setup 
+} 
+
+void loop()  { 
+  dimLed(ledR);
+  dimLed(ledG);
+  dimLed(ledB);
+  dimLed(ledY);                        
+  } 
+void dimLed(int led) {
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) {
+    analogWrite(led, fadeValue);
+    delay(dimTime);
+  }
+  for(int fadeValue = 255 ; fadeValue >=0; fadeValue -=5) {
+    analogWrite(led, fadeValue);
+    delay(dimTime);
+  }
+}
